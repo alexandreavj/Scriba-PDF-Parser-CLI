c293bf3922abd955b866058f2126e3e1f3d2f8d5
Alexandre Afonso Vaz Jacob <alexandrejacob@student.dei.uc.pt>
2026-03-15 21:35:13 +0000

    testing main

--- Scriba PDF Parser/main.swift
+++ Scriba PDF Parser/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
 
+func main() {
+    print("<=== Scriba PDF Parser CLI ===>")
+    
+    print("PDF file path: ", terminator: "")
+    guard let pdfPath = readLine(), !pdfPath.trimmingCharacters(in: .whitespaces).isEmpty else {
+        print("Error while obtaining user input.")
+        return
+    }
+    
+    let pdfURL = URL(fileURLWithPath: pdfPath)
+    let textExtractor: TextExtractor = TextExtractor(pdfURL: pdfURL)
+    
+    do {
+        let text: NSMutableAttributedString = try textExtractor.extractText()
+        print(text.string)
+    } catch {
+        fputs("Extraction failed: \(error)\n", stderr)
+    }
+
+}
+
+main()
