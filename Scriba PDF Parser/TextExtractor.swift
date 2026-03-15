5ef28f20850a54eca9790ca6180a399e6d9986bf
Alexandre Afonso Vaz Jacob <alexandrejacob@student.dei.uc.pt>
2026-03-15 21:17:20 +0000

    TextExtractor struct with function for text extraction through PDF parsing - extractText()

---
+++ Scriba PDF Parser/TextExtractor.swift
@@ -0,0 +1,35 @@
+//
+//  TextExtractor.swift
+//  Scriba PDF Parser
+//
+//  Created by Alexandre Jacob on 15/03/2026.
+//
+
+import PDFKit
+
+
+struct TextExtractor {
+    let pdfURL: URL
+    
+    func extractText() throws -> (String, NSMutableAttributedString) {
+        if let document = PDFDocument(url: self.pdfURL) {
+            let pageCount = document.pageCount
+            let documentContent = NSMutableAttributedString()
+
+            for i in 0 ..< pageCount {
+                guard let page = document.page(at: i) else { continue }
+                guard let pageContent = page.attributedString else { continue }
+                documentContent.append(pageContent)
+            }
+            
+            return (documentContent.string, documentContent)
+        } else {
+            throw ExtractionError.unableToLoadPDF(message: "Could not load PDF file with path \(self.pdfURL.path).");
+        }
+    }
+    
+    
+    enum ExtractionError: Error {
+        case unableToLoadPDF(message: String)
+    }
+}
