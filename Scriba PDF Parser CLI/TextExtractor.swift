1dd7527466ea0621c8dfa73e7dc9e216eeb364fd
Alexandre Afonso Vaz Jacob <alexandrejacob@student.dei.uc.pt>
2026-03-17 00:41:56 +0000

    update extractText() to return AttributedString per page

--- Scriba PDF Parser CLI/TextExtractor.swift
+++ Scriba PDF Parser CLI/TextExtractor.swift
@@ -11,15 +11,23 @@ import PDFKit
 struct TextExtractor {
     let pdfURL: URL
     
-    func extractText() throws -> NSMutableAttributedString {
+    func extractText() throws -> [NSMutableAttributedString] {
         if let document = PDFDocument(url: self.pdfURL) {
             let pageCount = document.pageCount
-            let documentContent = NSMutableAttributedString()
+            var documentContent: [NSMutableAttributedString] = []
 
             for i in 0 ..< pageCount {
-                guard let page = document.page(at: i) else { continue }
-                guard let pageContent = page.attributedString else { continue }
-                documentContent.append(pageContent)
+                documentContent.append(NSMutableAttributedString())
+                
+                guard let page = document.page(at: i) else {
+                    print("Could not load page \(i+1).")
+                    continue
+                }
+                guard let pageContent = page.attributedString else {
+                    print("Could not parse text from page \(i+1).")
+                    continue
+                }
+                documentContent[i].append(pageContent)
             }
             
             return documentContent
